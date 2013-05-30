736ac0086e6197b6c67f2199b00b3e41a8398be1
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-05-30 19:46:23 +0400

    disable migration generation if exist

--- lib/generators/redditor/install/install_generator.rb
+++ lib/generators/redditor/install/install_generator.rb
@@ -27,12 +27,25 @@ module Redditor
       end
 
       def generate_migration
-        migration_template "migrations/page.rb", "db/migrate/redditor_create_pages.rb"
-        migration_template "migrations/video_block.rb", "db/migrate/redditor_create_video_blocks.rb"
-        migration_template "migrations/text_block.rb", "db/migrate/redditor_create_text_blocks.rb"
-        migration_template "migrations/slider_block.rb", "db/migrate/redditor_create_slider_blocks.rb"
+        migration_template "migrations/page.rb", "db/migrate/redditor_create_pages.rb" unless migration_exists?("pages")
+        migration_template "migrations/video_block.rb", "db/migrate/redditor_create_video_blocks.rb" unless migration_exists?("video_blocks")
+        migration_template "migrations/text_block.rb", "db/migrate/redditor_create_text_blocks.rb" unless migration_exists?("text_blocks")
+        migration_template "migrations/slider_block.rb", "db/migrate/redditor_create_slider_blocks.rb" unless migration_exists?("slider_blocks")
         # migration_template "migrations/image.rb", "db/migrate/redditor_create_images.rb"
       end
+
+      private
+
+        def migration_exist? name
+        end
+
+        def migration_exists?(name)
+          Dir.glob("#{File.join(destination_root, migration_path)}/[0-9]*_*.rb").grep(/\d+_redditor_create_#{name}.rb$/).first
+        end
+
+        def migration_path
+          @migration_path ||= File.join("db", "migrate")
+        end
 
     end
   end
