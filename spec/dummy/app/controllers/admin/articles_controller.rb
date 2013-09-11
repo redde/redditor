d08352af3faf99b5a9ea7e8f3c820bce6aaa31e3
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-09-11 17:01:14 +0400

    Temp. Fixed templates names

--- spec/dummy/app/controllers/admin/articles_controller.rb
+++ spec/dummy/app/controllers/admin/articles_controller.rb
@@ -4,14 +4,20 @@ class Admin::ArticlesController < Admin::BaseController
 
   def edit
     @article = Article.find(params[:id])
+    @article.build_page.save unless @article.page
   end
 
   def update
     @article = Article.find(params[:id])
-    @article.update_attributes(article_attributes)
-    render "edit"
+    @article.update_attributes(article_params)
+    redirect_to [:edit, :admin, @article]
   end
 
   def destroy
   end
+
+  private
+    def article_params
+      params.require(:article).permit!
+    end
 end
--- spec/dummy/app/controllers/admin/base_controller.rb
+++ spec/dummy/app/controllers/admin/base_controller.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 
 class Admin::BaseController < ActionController::Base
+  layout 'application'
   
 end
