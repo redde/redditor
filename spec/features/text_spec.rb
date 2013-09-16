949ed4ef878f3b949b7a8c4b447adf5dc6ff1349
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-09-16 21:11:07 +0400

    Fixed all images in css

--- spec/features/text_spec.rb
+++ spec/features/text_spec.rb
@@ -9,7 +9,9 @@ describe "Text block" do
 
   it "Saves text block to article", type: :feature, js: true do
     visit "/admin/articles/#{article.id}/edit"
-    save_page
-    find_link(I18n.t("redditor.add.text_block")).trigger('click')
+    find_link(I18n.t("redditor.add.text_block")).trigger("click")
+    page.find("textarea").set("test text block")
+    click "submit"
+    expect(page).to have_content "test text block"
   end
 end
