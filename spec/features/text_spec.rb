13a1157f0401aa4608ed7c5766bd21d35ac4a850
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-09-16 23:20:29 +0400

    Added spec with saving with ok button

--- spec/features/text_spec.rb
+++ spec/features/text_spec.rb
@@ -12,7 +12,19 @@ describe "Text block" do
     find_link(I18n.t("redditor.add.text_block")).click
     page.find("textarea").set("test text block")
     find_button("Submit").click
-    save_page
+    expect(page.find("textarea").value).to eq "test text block"
+  end
+
+  it "Shows validation error if text block content is empty", type: :feature, js: true do
+    pending
+  end
+
+  it "Saves text block to article on save button", type: :feature, js: true do
+    visit "/admin/articles/#{article.id}/edit"
+    find_link(I18n.t("redditor.add.text_block")).click
+    page.find("textarea").set("test text block")
+    find_link("✔").click
+    visit "/admin/articles/#{article.id}/edit"
     expect(page.find("textarea").value).to eq "test text block"
   end
 end
