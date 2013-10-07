9c55d60b139095acf22689ffa969ad9d66708d5c
Konstantin Gorozhankin <konstantin.gorozhankin@gmail.com>
2013-10-07 18:30:26 +0400

    fileapi remove old folder

--- app/assets/javascripts/redditor/fileapi/mailru.js
+++
@@ -1,6 +0,0 @@
-//= require redditor/fileapi/config
-//= require redditor/fileapi/FileAPI.min
-//= require redditor/fileapi/FileAPI.id3.js
-//= require redditor/fileapi/FileAPI.exif.js
-//= require redditor/fileapi/tmpl
-//= require redditor/fileapi/uploader
