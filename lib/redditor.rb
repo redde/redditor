c6e7f53e6701f474e8ed64e463602ff2f2f8b1d7
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-11-15 01:07:34 +0400

    Fixed requirements

--- lib/redditor.rb
+++ lib/redditor.rb
@@ -1,9 +1,17 @@
 require "redditor/engine"
-
 require 'redditor/has_redditor'
-
 require "generators/redditor/views/views_generator"
 require 'rails/generators/active_record'
+require 'carrierwave/orm/activerecord'
+
+require_relative '../app/uploaders/redditor_uploader'
+require_relative '../app/models/redditor/page'
+require_relative '../app/models/redditor/content_block'
+require_relative '../app/uploaders/redditor_uploader'
+require_relative '../app/models/redditor/image'
+require_relative '../app/models/redditor/slider_block'
+require_relative '../app/models/redditor/text_block'
+require_relative '../app/models/redditor/video_block'
 
 module Redditor
 end
