473cef93eec6527d59c2005365a3ac789bcce0cb
Oleg Bovykin <oleg.bovykin@gmail.com>
2014-01-28 20:21:06 +0400

    Removed locales deprecation warning

--- redditor.gemspec
+++ redditor.gemspec
@@ -23,7 +23,6 @@ Gem::Specification.new do |s|
   
   s.add_development_dependency 'capybara'
   s.add_development_dependency 'poltergeist'
-  s.add_development_dependency "haml-rails"
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "rspec-rails"
   s.add_development_dependency "guard-rspec"
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -19,6 +19,8 @@ module Dummy
       g.template_engine :haml
     end
 
+    I18n.enforce_available_locales = false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
